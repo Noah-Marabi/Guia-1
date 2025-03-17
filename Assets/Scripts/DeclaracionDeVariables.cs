3d4c1452c10a5f423af058c5504c1c8d16892710
Noah-Marabi <noahmarab@gmail.com>
2025-03-17 10:41:50 -0300

    Ejercicio 1 terminado
    termine toda la 1

--- Assets/Scripts/DeclaracionDeVariables.cs
+++ Assets/Scripts/DeclaracionDeVariables.cs
@@ -12,7 +12,17 @@ public class DeclaracionDeVariables : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //Primeras variables
+        edadUsuario = 16;
+        sueldoUsuario = 10000.00f;
+        domicilio = "Hipolito Yrigoyen 650";
+        estaAprobado = false;
 
+        // Las muestro en consola
+        Debug.Log(edadUsuario);
+        Debug.Log(sueldoUsuario);
+        Debug.Log(domicilio);
+        Debug.Log(estaAprobado);
     }
 
     // Update is called once per frame
