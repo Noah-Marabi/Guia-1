7c0f7f6b266f3b68d7187506163f5f085a074135
Noah-Marabi <noahmarab@gmail.com>
2025-03-17 10:28:42 -0300

    variables creadas
    creamos las primeras variables asi que no hay mucho que explicar

--- Assets/Scripts/DeclaracionDeVariables.cs
+++ Assets/Scripts/DeclaracionDeVariables.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class DeclaracionDeVariables : MonoBehaviour
 {
+    int edadUsuario;
+    float sueldoUsuario;
+    string domicilio;
+    bool estaAprobado;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
