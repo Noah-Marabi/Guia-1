25d1e30528a6ca97ac775af1139bcfa01045672f
Noah-Marabi <noahmarab@gmail.com>
2025-04-03 10:00:21 -0300

    ejercicio 5 division prohibida
    no se puede dividir entre 0

---
+++ Assets/Scripts/DivisionProhibida.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DivisionProhibida : MonoBehaviour
+{
+    public int Num1;
+    public int Num2;
+    float resultado;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(Num2 == 0)
+        {
+            //instrucciones por true
+            Debug.Log("No se puede dividir por cero");
+        }else
+        {
+            //instrucciones por false
+            resultado = Num1 / Num2;
+            Debug.Log(Num1 + "/" + Num2 + " = " + resultado);
+        }
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
