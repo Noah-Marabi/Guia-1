be77361bc75c0f06b27442436883029c66dc1039
Noah-Marabi <noahmarab@gmail.com>
2025-04-07 10:40:45 -0300

    SOLO HICE LO PRIMERO DE COMIDAS DE ANIMALES

---
+++ Assets/Scripts/ComidaDeAnimalitos.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComidaDeAnimalitos : MonoBehaviour
+{
+    //Declaramos Variables
+
+   
+    public string Animal;
+    public int cantidadDeDias;
+    float costoAlimento;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      //Validacion de Datos
+
+        if ( cantidadDeDias < 3)
+        {
+            Debug.Log("Se debe de ingresar una catidad de dias mayor a 3");
+            return;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
